71d46360c38a2ecfd6b66fc2664744e4da492006
Haico27 <haico.wensink@gmail.com>
2017-02-19 22:26:02 +0100

    added handleclick function and completed setting state of squares in board

--- src/components/Board.js
+++ src/components/Board.js
@@ -11,8 +11,14 @@ class Board extends React.PureComponent {
   }
 
   renderSquare(i) {
-    return <Square value={this.state.squares[i]} />;
+    return <Square value={this.state.squares[i]} onClick={() => this.handleClick(i)} />
   }
+
+  handleClick(i) {
+  const squares = this.state.squares.slice()
+  squares[i] = 'X'
+  this.setState({squares: squares})
+}
 
   render() {
     const status = 'Next player: X';
