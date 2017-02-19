9fc5e38f31f1144010771cbfdcaec41978661c62
hansvanatten <hansvanatten@gmail.com>
2017-02-19 23:57:03 +0100

    game editor

---
+++ src/games/GameEditor.js
@@ -0,0 +1,95 @@
+import React, { PureComponent } from 'react'
+import Editor from 'react-medium-editor'
+import toMarkdown from 'to-markdown'
+import { connect } from 'react-redux'
+import 'medium-editor/src/sass/medium-editor.scss'
+import CREATE_GAME  from '../actions/games/create-game'
+import './GameEditor.sass'
+import saveGame from '../actions/games/save-game'
+
+const TYPES = [
+  'Beginner',
+  'Normal',
+  'Pro',
+]
+
+class GameEditor extends PureComponent {
+  constructor(props) {
+    super()
+
+    const { title, beginner, normal, pro } = props
+
+    this.state = {
+      title,
+      beginner,
+      normal,
+      pro,
+    }
+  }
+
+  updateTitle(event) {
+    if (event.keyCode === 13) {
+      event.preventDefault()
+      this.refs.summary.medium.elements[0].focus()
+    }
+    this.setState({
+      title: this.refs.title.value
+    })
+  }
+
+  setType(event) {
+    this.setState({
+      beginner: event.target.value === 'Beginner',
+      normal: event.target.value === 'Normal',
+      pro: event.target.value === 'Pro'
+    })
+  }
+
+  saveGame() {
+    const {
+      title,
+      beginner,
+      normal,
+      pro,
+    } = this.state
+
+    const game = {
+      title,
+      beginner,
+      normal,
+      pro,
+    }
+
+this.props.CREATE_GAME(game)
+
+    console.log(game)
+  }
+
+  render() {
+    return (
+      <div className="editor">
+        <input
+          type="text"
+          ref="title"
+          className="title"
+          placeholder="Title"
+          defaultValue={this.state.title}
+          onChange={this.updateTitle.bind(this)}
+          onKeyDown={this.updateTitle.bind(this)} />
+
+        {TYPES.map((type) => {
+          return <label key={type} htmlFor={type}>
+            <input id={type} type="radio" name="type" value={type} onChange={this.setType.bind(this)} />
+            {type}
+          </label>
+        })}
+
+        <div className="actions">
+          <button className="primary" onClick={this.saveGame.bind(this)}>Save</button>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default connect(null, { CREATE_GAME, saveGame }) (GameEditor)
